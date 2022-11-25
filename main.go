f288de87699a7c75da671fce40b4ad1a6a0703c2
Danny Festor <danny.festor@gmail.com>
2022-11-25 12:20:06 +0900

    Make a custom Route Handler Type

--- main.go
+++ main.go
@@ -19,7 +19,20 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:danny@festor.info\">danny@festor.info</a>.")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
+// func pathHandler(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	default:
+// 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+// 	}
+// }
+
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
@@ -32,10 +45,13 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	// http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/", pathHandler)
+	// http.HandleFunc("/", pathHandler)
 	// http.HandleFunc("/contact", contactHandler)
 	// http.HandleFunc("/path", pathHandler)
 	fmt.Println("Starting the server on :3000 ...")
 
-	http.ListenAndServe(":3000", nil)
+	// http.ListenAndServe(":3000", nil)
+
+	var router Router
+	http.ListenAndServe(":3000", router)
 }
