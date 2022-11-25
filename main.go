e3b80d5115df25c912d82f4bc443454a5718ca03
Danny Festor <danny.festor@gmail.com>
2022-11-25 11:54:20 +0900

    Just testing some Request Parameters

--- main.go
+++ main.go
@@ -19,9 +19,14 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:danny@festor.info\">danny@festor.info</a>.")
 }
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, r.URL.Path)
+}
+
 func main() {
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/path", pathHandler)
 	fmt.Println("Starting the server on :3000 ...")
 
 	http.ListenAndServe(":3000", nil)
