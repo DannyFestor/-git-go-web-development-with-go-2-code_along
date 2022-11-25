b3376bd63cebdd21a85ea48ec0671cfa18438f7f
Danny Festor <danny.festor@gmail.com>
2022-11-25 11:48:32 +0900

    Add simple contact page mock

--- main.go
+++ main.go
@@ -8,14 +8,20 @@ import (
 // added modd for dynamic reloading
 // go install github.com/cortesi/modd/cmd/modd@latest
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	// w.Header().Set("Content-Type", "text/plain")
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8") // actually not needed as go tries to auto detect the type
+	fmt.Fprintf(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:danny@festor.info\">danny@festor.info</a>.")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000 ...")
 
 	http.ListenAndServe(":3000", nil)
