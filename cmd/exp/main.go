2b55b99d9829321424c21d405ea44a284c70f3f1
Danny Festor <danny.festor@gmail.com>
2022-11-29 16:03:12 +0900

    Experiment with PSQL Connection

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,42 +1,23 @@
 package main
 
 import (
-	"html/template"
-	"os"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
-type User struct {
-	Name      string
-	Bio       string // template.HTML would not be escaped
-	Age       uint32
-	Weight    float32
-	Hobbies   []string
-	Married   bool
-	Knowledge map[string]string
-}
-
 func main() {
-	t, err := template.ParseFiles("hello.gohtml")
+	db, err := sql.Open("pgx", "host=localhost port=5432 user=lenslocked password=lenslocked dbname=lenslockedgo get sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
 	if err != nil {
 		panic(err)
 	}
 
-	user := User{
-		Name:    "Danny Festor",
-		Bio:     `<script>alert('HAHAHA')</script>`,
-		Age:     38,
-		Weight:  88.5,
-		Hobbies: []string{"Programming", "Bicycle", "Eat"},
-		Married: true,
-		Knowledge: map[string]string{
-			"PHP":        "good",
-			"Go":         "I'm learning, ok?",
-			"Javascript": "cool",
-		},
-	}
-
-	err = t.Execute(os.Stdout, user)
-	if err != nil {
-		panic(err)
-	}
+	fmt.Println("Connected to PSQL")
 }
