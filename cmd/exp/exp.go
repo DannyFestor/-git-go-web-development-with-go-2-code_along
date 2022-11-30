b73ce19fa7240c103223125f9cb48a919f76ab63
Danny Festor <danny.festor@gmail.com>
2022-11-30 13:57:08 +0900

    Make PSQL Config

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -1,43 +1,14 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 
-	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/danakin/web-dev-with-go-2-code_along/models"
 )
 
-type PostgresConfig struct {
-	Host     string
-	Port     string
-	User     string
-	Password string
-	Database string
-	SSLMode  string
-}
-
-func (cfg PostgresConfig) String() string {
-	return fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-		cfg.Host,
-		cfg.Port,
-		cfg.User,
-		cfg.Password,
-		cfg.Database,
-		cfg.SSLMode,
-	)
-}
-
 func main() {
-	cfg := PostgresConfig{
-		Host:     "localhost",
-		Port:     "5432",
-		User:     "lenslocked",
-		Password: "lenslocked",
-		Database: "lenslocked",
-		SSLMode:  "disable",
-	}
-	db, err := sql.Open("pgx", cfg.String())
+	cfg := models.DefaultPostgresConfig()
+	db, err := models.Open(cfg)
 	if err != nil {
 		panic(err)
 	}
@@ -47,111 +18,14 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Connected to PSQL")
+	fmt.Println("Connected!")
 
-	// Create a table
-	_, err = db.Exec(`
-	CREATE TABLE IF NOT EXISTS users (
-		id SERIAL PRIMARY KEY,
-		email VARCHAR (255) UNIQUE NOT NULL,
-		name TEXT
-	);
-
-	CREATE TABLE IF NOT EXISTS orders (
-		id SERIAL PRIMARY KEY,
-		user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-		amount INT,
-		description TEXT
-	);
-	`)
+	us := models.UserService{
+		DB: db,
+	}
+	user, err := us.Create("bob4@bob.com", "bob123")
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Println("Tables Created")
-
-	// name := "Danny Festor"
-	// email := "denakino@gmail.com"
-	// // _, err = db.Exec(`
-	// // INSERT INTO users(name, email)
-	// // VALUES ($1, $2);
-	// // `, name, email)
-	// row := db.QueryRow(`
-	// INSERT INTO users(name, email)
-	// VALUES ($1, $2)
-	// RETURNING id;
-	// `, name, email)
-	// var id int
-	// err = row.Scan(&id)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println("User created with ID", id)
-
-	// query := `
-	// SELECT name, email
-	// FROM users
-	// WHERE id=$1;
-	// `
-	// id := 1
-	// row := db.QueryRow(query, id)
-	// var name, email string
-	// err = row.Scan(&name, &email)
-	// if err != nil {
-	// 	if err == sql.ErrNoRows {
-	// 		fmt.Println("Error, no rows")
-	// 		return
-	// 	}
-	// 	panic(err)
-	// }
-	// fmt.Printf("User information: name=%s email=%s\n", name, email)
-
-	// for i := 1; i <= 5; i++ {
-	// 	amount := i * 100
-	// 	desc := fmt.Sprintf("Fake order #%d", i)
-	// 	_, err := db.Exec(`
-	// 	INSERT INTO orders (user_id, amount, description)
-	// 	VALUES ($1, $2, $3);
-	// 	`, id, amount, desc)
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// }
-
-	userID := 1
-
-	type Order struct {
-		ID          int
-		UserID      int
-		Amount      int
-		Description string
-	}
-
-	var orders []Order
-	query := `
-	SELECT id, amount, description
-	FROM orders
-	WHERE user_id = $1
-	`
-	rows, err := db.Query(query, userID)
-	if err != nil {
-		panic(err)
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var order Order
-		order.UserID = userID
-		err = rows.Scan(&order.ID, &order.Amount, &order.Description)
-		if err != nil {
-			panic(err)
-		}
-		orders = append(orders, order)
-	}
-
-	if rows.Err() != nil {
-		panic(rows.Err())
-	}
-
-	fmt.Println("Orders:", orders)
+	fmt.Println(user)
 }
--- models/user.go
+++ models/user.go
@@ -32,7 +32,7 @@ func (us *UserService) Create(email, password string) (*User, error) {
 	}
 	row := us.DB.QueryRow(`
 		INSERT INTO users (email, password_hash)
-		VALUES ($,1 $2)
+		VALUES ($1, $2)
 		RETURNING id
 	`, email, passwordHash)
 	err = row.Scan(&user.ID)
