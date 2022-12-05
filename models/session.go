42d52d91ce2c03ea807f06f809684ba3d540ec8a
Danny Festor <danny.festor@gmail.com>
2022-12-05 17:48:15 +0900

    Add Session Service Method Stubs

--- models/session.go
+++ models/session.go
@@ -1,7 +1,25 @@
 package models
 
+import "database/sql"
+
 type Session struct {
 	ID        int
 	UserId    int
+	Token     string // only set when creating a new session, not filled when looking up a session
 	TokenHash string
+}
+
+type SessionService struct {
+	DB *sql.DB // TODO: Redis?
+}
+
+func (ss *SessionService) Create(userID int) (*Session, error) {
+	// TODO
+	// 1. Create Session Token
+	return nil, nil
+}
+
+func (ss *SessionService) User(token string) (*User, error) {
+	// TODO
+	return nil, nil
 }
