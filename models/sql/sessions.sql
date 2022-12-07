36700bb845b7fd0d8c2b7ab4df82f0d47cfcb36e
Danny Festor <danny.festor@gmail.com>
2022-12-07 16:48:04 +0900

    Remove unused SQL Files

--- models/sql/sessions.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE sessions (
-	id SERIAL PRIMARY KEY,
-    user_id INT UNIQUE REFERENCES users (id) ON DELETE CASCADE, -- references part alternative below
-    token_hash TEXT UNIQUE NOT NULL--,
-    --FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE -- alternative above
-);
