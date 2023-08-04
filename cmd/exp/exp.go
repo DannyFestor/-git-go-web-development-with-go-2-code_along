49b2359ce6b6222e44f7761e446ae6933b106d45
Danny Festor <danny@festor.info>
2023-08-04 17:26:39 +0900

    Add .env

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -2,25 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 
 	"github.com/danakin/web-dev-with-go-2-code_along/models"
-)
-
-const (
-	host     = "localhost" // sandbox.smtp.mailtrap.io etc.
-	port     = 1025        // standard port is 587
-	username = "mailpit"   // mailtrap username
-	password = "mailpit"   // mailtrap password
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	host := os.Getenv("MAIL_HOST")
+	port, err := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	if err != nil {
+		panic(err)
+	}
+	username := os.Getenv("MAIL_USERNAME")
+	password := os.Getenv("MAIL_PASSWORD")
+
 	es := models.NewEmailService(models.SMTPConfig{
 		Host:     host,
 		Port:     port,
 		Username: username,
 		Password: password,
 	})
-	err := es.ForgotPassword("danny@festor.info", "https://localhost:3000")
+	err = es.ForgotPassword("danny@festor.info", "https://localhost:3000")
 	if err != nil {
 		panic(err)
 	}
--- go.mod
+++ go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/jackc/pgproto3/v2 v2.3.1 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	github.com/jackc/pgtype v1.12.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lib/pq v1.10.6 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pressly/goose/v3 v3.7.0 // indirect
