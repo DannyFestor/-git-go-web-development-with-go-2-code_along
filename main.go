7b85dd86e30118eaee9b4675850199a3955817ec
Danny Festor <danny@festor.info>
2023-11-14 16:59:31 +0900

    Initialize Services With Env Vars

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/danakin/web-dev-with-go-2-code_along/controllers"
 	"github.com/danakin/web-dev-with-go-2-code_along/migrations"
@@ -12,15 +14,20 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/gorilla/csrf"
+	"github.com/joho/godotenv"
 )
 
 // added modd for dynamic reloading
 // go install github.com/cortesi/modd/cmd/modd@latest
 
 func main() {
+	cfg, err := loadEnvConfig()
+	if err != nil {
+		panic(err)
+	}
+
 	// Set Up DB Connections
-	cfg := models.DefaultPostgresConfig()
-	db, err := models.Open(cfg)
+	db, err := models.Open(cfg.PSQL)
 	if err != nil {
 		panic(err)
 	}
@@ -38,30 +45,38 @@ func main() {
 	fmt.Println("Connected!")
 
 	// Set Up Services
-	userService := models.UserService{
+	userService := &models.UserService{
 		DB: db,
 	}
-	sessionService := models.SessionService{
+
+	sessionService := &models.SessionService{
 		DB:            db,
 		BytesPerToken: 32,
 	}
 
-	// Set Up Middleware
-	userMiddleware := controllers.UserMiddleware{
-		SessionService: &sessionService,
+	passwordResetService := &models.PasswordResetService{
+		DB: db,
 	}
 
-	csrfKey := "gFvi45R4fy5xNBlnEeZtQbfAVCYEIAUX"
+	emailService := models.NewEmailService(cfg.SMTP)
+
+	// Set Up Middleware
+	userMiddleware := controllers.UserMiddleware{
+		SessionService: sessionService,
+	}
+
+	csrfKey := cfg.CSRF.Key
 	csrfMiddleware := csrf.Protect(
 		[]byte(csrfKey),
-		// TODO: set to true for HTTPS production ready code
-		csrf.Secure(false),
+		csrf.Secure(cfg.CSRF.Secure),
 	)
 
 	// Set Up Controller
 	userController := controllers.User{
-		UserService:    &userService,
-		SessionService: &sessionService,
+		UserService:          userService,
+		SessionService:       sessionService,
+		PasswordResetService: passwordResetService,
+		EmailService:         emailService,
 	}
 	userController.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
 	userController.Templates.Login = views.Must(views.ParseFS(templates.FS, "login.gohtml", "tailwind.gohtml"))
@@ -104,6 +119,51 @@ func main() {
 	})
 
 	// Start the Server
-	fmt.Println("Starting the server on :3000 ...")
-	http.ListenAndServe(":3000", r)
+	fmt.Printf("Starting the server on %s ..\n", cfg.Server.Address)
+	err = http.ListenAndServe(cfg.Server.Address, r)
+	if err != nil {
+		panic(err)
+	}
+}
+
+type config struct {
+	PSQL models.PostgresConfig
+	SMTP models.SMTPConfig
+	CSRF struct {
+		Key    string
+		Secure bool
+	}
+	Server struct {
+		Address string
+	}
+}
+
+func loadEnvConfig() (config, error) {
+	var cfg config
+
+	err := godotenv.Load()
+	if err != nil {
+		return cfg, err
+	}
+
+	// PSQL Setup // TODO: get from .env
+	cfg.PSQL = models.DefaultPostgresConfig()
+
+	// SMTP Setup
+	cfg.SMTP.Host = os.Getenv("MAIL_HOST")
+	cfg.SMTP.Port, err = strconv.Atoi(os.Getenv("MAIL_PORT"))
+	if err != nil {
+		panic(err)
+	}
+	cfg.SMTP.Username = os.Getenv("MAIL_USERNAME")
+	cfg.SMTP.Password = os.Getenv("MAIL_PASSWORD")
+
+	// CSRF Setup // TODO: get from .env
+	cfg.CSRF.Key = "gFvi45R4fy5xNBlnEeZtQbfAVCYEIAUX"
+	cfg.CSRF.Secure = false
+
+	// Server Setup // TODO: get from .env
+	cfg.Server.Address = ":3000"
+
+	return cfg, nil
 }
