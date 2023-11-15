f3774e9c12292627323a1a819ac46438013ed397
Danny Festor <danny@festor.info>
2023-11-15 16:11:35 +0900

    Password Reset Consume

--- models/password_reset.go
+++ models/password_reset.go
@@ -85,8 +85,51 @@ func (service *PasswordResetService) Create(email string) (*PasswordReset, error
 	return &pwReset, nil
 }
 
-func (service *PasswordResetService) Consume(email string) (*User, error) {
-	return nil, fmt.Errorf("TODO: Implement PasswordResetService.Consume")
+func (service *PasswordResetService) Consume(token string) (*User, error) {
+	tokenHash := service.hash(token)
+	var user User
+	var pwReset PasswordReset
+
+	query := `
+	SELECT
+		password_resets.id,
+		password_resets.expires_at,
+		users.id,
+		users.email,
+		users.password_hash
+	FROM password_resets
+		JOIN users ON users.id = password_resets.user_id
+	WHERE
+		password_resets.token_hash = $1;
+	`
+	row := service.DB.QueryRow(query, tokenHash)
+	err := row.Scan(&pwReset.ID, &pwReset.ExpiresAt, &user.ID, &user.Email, &user.PasswordHash)
+	if err != nil {
+		return nil, fmt.Errorf("consume: %w", err)
+	}
+
+	if time.Now().After(pwReset.ExpiresAt) {
+		return nil, fmt.Errorf("token expired: %v", token)
+	}
+
+	err = service.delete(pwReset.ID)
+	if err != nil {
+		return nil, fmt.Errorf("consume: %w", err)
+	}
+
+	return &user, nil
+}
+
+func (service *PasswordResetService) delete(id int) error {
+	_, err := service.DB.Exec(`
+		DELETE FROM password_resets
+		WHERE id = $1;
+	`, id)
+	if err != nil {
+		return fmt.Errorf("delete: %w", err)
+	}
+
+	return nil
 }
 
 func (service *PasswordResetService) hash(token string) string {
