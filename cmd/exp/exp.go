1b25290da53040ba740a40cfcee93ea8a7203cf7
Danny Festor <danny@festor.info>
2023-11-27 15:40:53 +0900

    Glob Images

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -2,37 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
-	"strconv"
 
 	"github.com/danakin/web-dev-with-go-2-code_along/models"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	host := os.Getenv("MAIL_HOST")
-	port, err := strconv.Atoi(os.Getenv("MAIL_PORT"))
-	if err != nil {
-		panic(err)
-	}
-	username := os.Getenv("MAIL_USERNAME")
-	password := os.Getenv("MAIL_PASSWORD")
-
-	es := models.NewEmailService(models.SMTPConfig{
-		Host:     host,
-		Port:     port,
-		Username: username,
-		Password: password,
-	})
-	err = es.ForgotPassword("danny@festor.info", "https://localhost:3000")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Sent!")
+	gs := models.GalleryService{}
+	fmt.Println(gs.Images(2))
 }
--- models/gallery.go
+++ models/gallery.go
@@ -5,7 +5,12 @@ import (
 	"errors"
 	"fmt"
 	"path/filepath"
+	"strings"
 )
+
+type Image struct {
+	Path string
+}
 
 type Gallery struct {
 	ID     int
@@ -124,6 +129,25 @@ func (service *GalleryService) Delete(gallery *Gallery) error {
 	return nil
 }
 
+func (service *GalleryService) Images(galleryID int) ([]Image, error) {
+	globPattern := filepath.Join(service.galleryDir(galleryID), "*")
+	allFiles, err := filepath.Glob(globPattern)
+	if err != nil {
+		return nil, fmt.Errorf("retrieving gallery images: %w", err)
+	}
+
+	var images []Image
+	for _, file := range allFiles {
+		if hasExtension(file, service.extensions()) {
+			images = append(images, Image{
+				Path: file,
+			})
+		}
+	}
+
+	return images, nil
+}
+
 func (service *GalleryService) galleryDir(id int) string {
 	imagesDir := service.ImagesDir
 	if imagesDir == "" {
@@ -131,4 +155,26 @@ func (service *GalleryService) galleryDir(id int) string {
 	}
 
 	return filepath.Join(imagesDir, fmt.Sprintf("gallery-%d", id))
+}
+
+func (service *GalleryService) extensions() []string {
+	return []string{
+		".png",
+		".jpg",
+		".jpeg",
+		".gif",
+	}
+}
+
+func hasExtension(file string, extensions []string) bool {
+	for _, ext := range extensions {
+		file = strings.ToLower(file)
+		ext = strings.ToLower(ext)
+
+		if filepath.Ext(file) == ext {
+			return true
+		}
+	}
+
+	return false
 }
